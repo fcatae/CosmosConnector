86d28409184d732d25f57de338e31e1271a4bd8d
Fabricio Catae <fcatae@microsoft.com>
2017-08-15 19:43:57 -0300

    Add configuration

---
+++ CosmosConnector/AppConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace CosmosConnector
+{
+    class AppConfiguration
+    {
+        public class AppConfigurationException : Exception
+        {
+            public AppConfigurationException(string message) : base(message)
+            {
+            }
+        }
+
+        private string _configName { get; }
+        private IConfigurationRoot _config;
+
+        public AppConfiguration(string configName)
+        {
+            _configName = configName;
+
+            var builder = new ConfigurationBuilder()
+                            .AddEnvironmentVariables(configName)
+                            .AddUserSecrets("CosmosConnector-20171508");
+
+            _config = builder.Build();
+        }
+
+        public string Get(string key)
+        {
+            string value = _config[key];
+
+            if (value == null || value == "")
+                throw new AppConfigurationException($"Configuration [{key}] is empty");
+
+            return value;
+        }
+    }
+}
